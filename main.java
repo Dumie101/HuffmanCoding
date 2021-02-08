d44939d29c91aadab258892113831c627b2a85f7
Dumie101 <36200378+Dumie101@users.noreply.github.com>
2021-02-08 19:11:54 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+//package D101;
+
+import java.io.IOException;
+
+public class HuffmanDemo {
+
+
+    public static void main( String[] arg ) throws IOException {
+        Huffman.encode();
+        Huffman.decode();
+
+    //  /Users/ichirobanskota/Desktop/Pokemon.txt
+    //  /Users/ichirobanskota/Desktop/Hello.txt
+    // /Users/ichirobanskota/IdeaProjects/Assignment4/Huffman.txt
+    // /Users/ichirobanskota/IdeaProjects/Assignment4/Encode.txt
+
+
+    }
+
+
+
+
+
+}
